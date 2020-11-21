1c4e89bf908fcf18b854a01747e40755d2ae7eb1
Abah <abahwisdom@gmail.com>
2020-11-21 17:12:00 +0100

    elephant sql working

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express= require('express');
 require('dotenv').config();
 const pool= require('./db');
 
+pool.connect(function(err) {
+    if(err) {
+      return console.error('could not connect to postgres', err);
+    }
+})
+
 pool.on('connect', () => {
     console.log('connected to database');
     });
